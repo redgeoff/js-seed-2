bea4368d80e2b1c918d744921b0ad19486362ac3
Geoff Cox <redgeoff@gmail.com>
2017-12-14 13:47:53 -0800

    test(browser): fix labels

--- test/browser.js
+++ test/browser.js
@@ -2,8 +2,8 @@ import './chai'
 import './node-and-browser'
 import Foo from '../src'
 
-describe('node', () => {
-  it('should test only in node', async () => {
+describe('browser', () => {
+  it('should test only in browser', async () => {
     // TODO: insert tests that can only be tested in the browser
     let foo = new Foo()
     let thing = await foo.bar()
